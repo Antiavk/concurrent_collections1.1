8914f1166d77f009f9140009b37b90b2582fb58e
Antiavk <avk_tp@mail.ru>
2022-08-10 15:35:56 +0300

    Initial commit

---
+++ src/Specialist.java
@@ -0,0 +1,34 @@
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class Specialist extends Thread {
+
+    @Override
+    public void run() {
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        while (true) {
+
+            Call call = Main.calls.poll();
+            if (call != null) {
+                answerTheCall(call);
+            } else {
+                break;
+            }
+        }
+    }
+
+    public void answerTheCall(Call call) {
+
+        System.out.println(Thread.currentThread().getName() + " ответил на " + call.toString());
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+        }
+        System.out.println(Thread.currentThread().getName() + " закончил " + call.toString());
+    }
+}
